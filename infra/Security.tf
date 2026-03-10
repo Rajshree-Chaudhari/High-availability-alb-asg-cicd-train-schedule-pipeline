d36f3e9430604b156bac72bf8381cf124f8f7fc2
Rajshree Chaudhari <83112543+RJ-Chaudhari@users.noreply.github.com>
2026-03-10 21:23:21 +0530

    Create security groups for ALB and application
    Added security groups for ALB and application with appropriate ingress and egress rules.

---
+++ infra/Security.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "alb_sg" {
+
+  name        = "alb-security-group"
+  description = "Allow HTTP traffic from internet"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+
+  name        = "app-security-group"
+  description = "Allow traffic only from ALB"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
